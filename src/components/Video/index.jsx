a7bee5657ee11ec31d73effa372a104e6137012c
Enrique <octepc.97@gmail.com>
2022-12-14 09:45:33 -0500

    made some visual changes realted with the play button

--- src/components/Video/index.jsx
+++ src/components/Video/index.jsx
@@ -9,21 +9,30 @@ import sampleVideo from "../../assets/BigBuckBunny_512kb.mp4";
 
 export const Video = ({ source }) => {
   const videoRef = useRef(null);
+  const buttonPlayReference = useRef(null);
 
   const togglePlay = () => {
     if (videoRef.current.paused || videoRef.current.ended) {
       videoRef.current.play();
+      buttonPlayReference.current.classList.add("invisible");
     } else {
       videoRef.current.pause();
+      buttonPlayReference.current.classList.remove("invisible");
     }
   };
 
   return (
     <div className="videoContainer">
-      <video loop poster={source} ref={videoRef} className="video">
+      <video
+        loop
+        poster={source}
+        ref={videoRef}
+        className="video"
+        onClick={togglePlay}
+      >
         <source src={sampleVideo} type="video/mp4" />
       </video>
-      <button onClick={togglePlay} className="video--button">
+      <button className="video--button" ref={buttonPlayReference}>
         <FaPlayCircle />
       </button>
       <ActionBar
