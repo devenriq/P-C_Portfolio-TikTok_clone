98b8c23b546047a17233105bf00a7bfdd15a5b69
Enrique <octepc.97@gmail.com>
2022-12-13 21:39:11 -0500

    added video feature

--- src/components/Video/index.jsx
+++ src/components/Video/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { ActionBar } from "../ActionBar/index";
 import { Description } from "../Description";
+import sampleVideo from "../../assets/BigBuckBunny_512kb.mp4";
 
 export const Video = ({ source }) => {
   return (
     <div>
-      <img src={source} alt="video" />
+      <video>
+        <source src={sampleVideo} type="video/mp4" />
+      </video>
+      <button>Play</button>
       <ActionBar
         profileImg={"https://picsum.photos/900"}
         likesCount={10}
