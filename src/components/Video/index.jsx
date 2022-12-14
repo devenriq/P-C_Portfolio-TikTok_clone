496ee354c7bb92e529d45f714e95168c045c9682
Enrique <octepc.97@gmail.com>
2022-12-13 21:59:44 -0500

    fixed the appereance

--- src/components/Video/index.jsx
+++ src/components/Video/index.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 import { ActionBar } from "../ActionBar/index";
 import { Description } from "../Description";
+import "./styles.css";
+
+import { FaPlayCircle } from "react-icons/fa";
+
 import sampleVideo from "../../assets/BigBuckBunny_512kb.mp4";
 
 export const Video = ({ source }) => {
+  const videoRef = useRef(null);
+
+  const togglePlay = () => {
+    if (videoRef.current.paused || videoRef.current.ended) {
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
+  };
+
   return (
-    <div>
-      <video>
+    <div className="videoContainer">
+      <video loop poster={source} ref={videoRef} className="video">
         <source src={sampleVideo} type="video/mp4" />
       </video>
-      <button>Play</button>
+      <button onClick={togglePlay} className="video--button">
+        <FaPlayCircle />
+      </button>
       <ActionBar
         profileImg={"https://picsum.photos/900"}
         likesCount={10}
