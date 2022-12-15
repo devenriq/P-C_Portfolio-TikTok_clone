ec6bd597e46357d6ba9f146c4f2d9a50e01e53e7
Enrique <octepc.97@gmail.com>
2022-12-14 21:54:47 -0500

    added the context component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { CommentsContextProvider } from "./components/context/CommentsContext";
+
 import { Video } from "./components/Video/index";
 import { Layout } from "./components/layout";
+import { Comments } from "./components/Comments";
 
 import sampleVideo from "./assets/BigBuckBunny_512kb.mp4";
 
 function App() {
   return (
     <Layout>
-      <Video source={sampleVideo} poster="https://picsum.photos/900" />
+      <CommentsContextProvider>
+        <Video source={sampleVideo} poster="https://picsum.photos/900" />
+        <Comments />
+      </CommentsContextProvider>
     </Layout>
   );
 }
